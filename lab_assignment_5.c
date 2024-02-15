393657c933c9baede65f46195420ba864bf718a5
Stick Hands <joshuai.paz@gmail.com>
2024-02-15 17:03:37 -0500

    Initial commit

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -9,36 +9,104 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+    int count = 0;
+    node* current = head;
 
-   return (len);
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+
+    return count;
 }
 
-// parses the string in the linkedList
+//  parses the string in the linkedList
 //  if the linked list is head -> |a|->|b|->|c|
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+    int stringLength = length(head);
+    // Allocate memory for the C-style string
+    char* result = (char*)malloc((stringLength + 1) * sizeof(char)); // +1 for the null terminator
+
+    if (result == NULL) 
+    {
+        // Handle memory allocation failure
+        printf("Memory allocation error with result.");
+    }
+
+    int i = 0;
+    node* current = head;
+
+    // Traverse the linked list and copy characters to the result string array.
+    while (current != NULL) {
+        result[i] = current->letter;
+        i++;
+        current = current->next;
+    }
+
+    result[stringLength] = '\0'; // Add null terminator
+
+    return result;
 }
 
 // inserts character to the linkedlist
 // f the linked list is head -> |a|->|b|->|c|
-// then insertChar(&head, 'x') will update the linked list as foolows:
+// then insertChar(&head, 'x') will update the linked list as follows:
 // head -> |a|->|b|->|c|->|x|
+
 void insertChar(node** pHead, char c)
 {
+    // Allocate memory for the new node
+    node* newEndNode = malloc(sizeof(struct node));
+
+    if(newEndNode == NULL)
+    {
+        // Handle memory allocation failure
+        printf("Memory allocation error with newEndNode.");
+    }
+    
+    newEndNode->letter = c;
+    newEndNode->next = NULL;
+
+    if(*pHead == NULL)
+    {
+        // If the list is empty, set the new node as the head.
+        *pHead = newEndNode;
+    }
+    else
+    {
+        // Traverse the list to find the end and add the new node 
+        node* traverseNode;
+        traverseNode = *pHead;
+
+        while(traverseNode->next != NULL)
+        {
+            traverseNode = traverseNode-> next;
+            
+        }
+
+        traverseNode->next = newEndNode;     
+    }
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+    node* current = *pHead;
+    node* nextNode;
+
+    // Traverse the list, free memory, and update pointers.
+    while (current != NULL) 
+    {
+        nextNode = current->next; 
+        free(current); 
+        current = nextNode; 
+    }
+    
+    *pHead = NULL; // Set the head to NULL after deleting all nodes
 }
+
 
 int main(void)
 {
